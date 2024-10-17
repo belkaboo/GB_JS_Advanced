d4b17304c0ce140d69716d85f87c3337032e4cc0
belka.boo <ibelka.boo@gmail.com>
2024-10-17 19:01:51 +0300

    1_sem_hw add 2 solution

--- 1_SEM_HW/1_sem_hw.js
+++ 1_SEM_HW/1_sem_hw.js
@@ -91,3 +91,67 @@ for (let element of musicCollection) {
 Клиент Мария заказала: Суши "Калифорния" и Пиццу "Маргарита".
 Клиент Ирина заказала: Чизкейк.
 */
+
+const chiefs = new Map();
+
+chiefs.set('Виктор', 'специализация: Пицца')
+    .set('Ольга', 'специализация: Суши')
+    .set('Дмитрий', 'специализация: Десерты');
+
+const dishes = new Map();
+
+dishes.set('Пицца "Маргарита"', 'Виктор')
+    .set('Пицца "Пепперони"', 'Виктор')
+    .set('Суши "Филадельфия"', 'Ольга')
+    .set('Суши "Калифорния"', 'Ольга')
+    .set('Тирамису', 'Дмитрий')
+    .set('Чизкейк', 'Дмитрий');
+
+
+
+const clientsOrders = [];
+
+
+function addClientsOrders(clientName, ...dishes) {
+    const client = clientsOrders.find(client => client.name === clientName);
+    if (client) {
+        client.orders.push([...dishes]);
+    }
+    else {
+        clientsOrders.push({ name: clientName, orders: [...dishes] });
+    }
+}
+
+
+function displayClients(orderQueue) {
+    console.log(`Всего клиентов: ${orderQueue.length}`);
+    for (let index = 0; index < orderQueue.length; index++) {
+        console.log(`Клиент ${index + 1} - ${orderQueue[index].name}`);
+    }
+
+}
+
+
+function displayOrders(orderQueue) {
+    orderQueue.forEach(client => {
+        console.log(`Клиент ${client.name} заказал: `);
+
+        client.orders.forEach(dish => {
+            console.log(`${dish} (Готовит: ${dishes.get(dish)})`);
+        });
+
+        console.log('---------');
+    });
+}
+
+
+addClientsOrders('Алексей', 'Пицца "Пепперони"', 'Тирамису');
+addClientsOrders('Мария', 'Суши "Калифорния"', 'Пицца "Маргарита"');
+addClientsOrders('Ирина', 'Чизкейк');
+
+
+displayClients(clientsOrders); // вывод клиентов
+
+displayOrders(clientsOrders); // вывод клиентских заказов с поворами
+
+
